3ac56cba2c15777e528cae869e61e4822dcc08bc
faradarsmz <faradarsmz@gmail.com>
2024-11-28 10:46:20 +0400

    Initial

--- src/dbank_frontend/src/App.jsx
+++ src/dbank_frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { dbank_backend } from 'declarations/dbank_backend';
 
 function App() {
   const [greeting, setGreeting] = useState('');
+  const [balance, setBalance] = useState(0);
+  const [amount, setAmount] = useState(0);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -13,17 +15,72 @@ function App() {
     return false;
   }
 
+
+  function handleCheckBalance() {
+    dbank_backend.checkBalance().then((currentBalance)=>{
+      setBalance(currentBalance);
+    })
+  }
+
+  useEffect(()=>{
+    handleCheckBalance();
+  },[])
+
+  function handleTopUp(event) {
+    event.preventDefault();
+    const topUpAmount = parseFloat(amount);
+    dbank_backend.topUp(topUpAmount).then(() =>{
+      setAmount(0);
+      handleCheckBalance();
+    })
+    return false;
+  }
+
+  function handleWithdraw(event) {
+    event.preventDefault();
+    const withdrawAmount = parseFloat(balance);
+    // const withdrawAmount = parseFloat(amount);
+    dbank_backend.withDraw(withdrawAmount).then(() =>{
+      setAmount(0);
+      handleCheckBalance();
+    })
+    return false;
+  }
+
+  function compound (event) {
+    event.preventDefault();
+    dbank_backend.compound().then(()=>{
+      handleCheckBalance();
+    })
+  }
+
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
       <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+
+      <h2>Current Balance: {balance}</h2>
+
+      <form onSubmit={handleTopUp}>
+      <label htmlFor="topup">Top Up: &nbsp;</label>
+      <input type="number" id='topup' value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <button type='submit'>Top Up</button>
       </form>
-      <section id="greeting">{greeting}</section>
+
+      <form onSubmit={handleWithdraw}>
+      <label htmlFor="withdraw">Withdraw: &nbsp;</label>
+      <input type="number" id='withdraw' value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <button type='submit'>Withdraw</button>
+      </form>
+
+      <form>
+        <label htmlFor="compound">Compound: &nbsp;</label>
+        <button onClick={compound}>Compound</button>
+      </form>
+
+
+
     </main>
   );
 }
